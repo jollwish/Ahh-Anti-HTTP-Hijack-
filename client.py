4a0ae84ad35a692ebf79f8ecdc801e221146e149
Yuping Luo <roosephu@gmail.com>
2018-01-15 15:20:57 -0500

    switch to https when available

--- client.py
+++ client.py
@@ -3,9 +3,17 @@ import struct
 import threading
 import socket
 import select
+import traceback
+from logger import getLogger
+
+from email import message_from_file
+from io import StringIO
+import requests
 
 from scapy.all import ETH_P_ALL
 from scapy.all import MTU
+
+logger = getLogger('client')
 
 PROXY_ADDR = "0.0.0.0"
 HTTP_PORT = 1082
@@ -17,123 +25,149 @@ RECV_SIZE = 65536
 SEND_SIZE = 50
 TIMEOUT = 10
 
+HTTPS_HOST = set(['www.cs.princeton.edu'])
+
 def ready_recv(sock):
-	sock.setblocking(0)
-	ready = select.select([sock], [], [], TIMEOUT)
-	if ready[0]:
-		return sock.recv(RECV_SIZE)
+    sock.setblocking(0)
+    ready = select.select([sock], [], [], TIMEOUT)
+    if ready[0]:
+        return sock.recv(RECV_SIZE)
 
 def pipe_recv(peer, remote):
-	#print('start recv')
-	while 1:
-		buffer = ready_recv(remote)
-		if not buffer:
-			break
-		print('received %d bytes' % len(buffer))
-		peer.sendall(buffer)
-	peer.close()
-	remote.close()
+    #logger.info('start recv')
+    while 1:
+        buffer = ready_recv(remote)
+        if not buffer:
+            break
+        logger.info('received %d bytes', len(buffer))
+        peer.sendall(buffer)
 
-def pipe_send(peer, remote):
-	#print('start send')
-	buffer = peer.recv(RECV_SIZE)
-	print('sending %d bytes' % len(buffer))
-	for i in range(0, len(buffer), SEND_SIZE):
-		remote.sendall(buffer[i : i+SEND_SIZE])
-	while 1:
-		buffer = ready_recv(peer)
-		if not buffer:
-			break
-		print('sending %d bytes' % len(buffer))
-		remote.sendall(buffer)
-	peer.close()
-	remote.close()
-	
+def make_HTTPS_request(http_req):
+    # logger.info(http_req)
+    req = http_req.decode('utf-8')
+    req, headers = req.split('\r\n', 1)
+    verb, uri = req.split(' ')[:2]
+    headers = message_from_file(StringIO(headers))
+
+    host = headers['Host']
+    if host in HTTPS_HOST:
+        r = requests.request(verb, "https://" + headers['Host'] + uri, headers=headers)
+        response = bytes("HTTP/1.1 %d %s\r\n" % (r.status_code, r.reason) + "".join(["%s: %s\r\n" % (k, v) for k, v in r.headers.items()]) + "\r\n" + r.text, 'utf-8')
+        return response
+    return None
+
+def pipe_send(peer, remote, buffer):
+    #logger.info('start send')
+    logger.info('sending %d bytes' % len(buffer))
+    for i in range(0, len(buffer), SEND_SIZE):
+        remote.sendall(buffer[i : i+SEND_SIZE])
+    while 1:
+        buffer = ready_recv(peer)
+        if not buffer:
+            break
+        logger.info('sending %d bytes' % len(buffer))
+        remote.sendall(buffer)
+    
 def run(peer):
-	
-	#Authentication
-	ver = peer.recv(1)
-	nmethods = peer.recv(1)[0]
-	for i in range(nmethods):
-		method = peer.recv(1)
-	peer.sendall(b'\x05\x00')
-	
-	print('Authentication succeeded.')
-	
-	#Connection
-	ver = peer.recv(1)
-	cmd = peer.recv(1)
-	rsv = peer.recv(1)
-	atyp = peer.recv(1)[0]
-	if atyp == 1: 
-		dst_addr = '%d.%d.%d.%d' % tuple(peer.recv(4))
-	elif atyp == 3:
-		len = peer.recv(1)[0]
-		domain_name = peer.recv(len).decode('iso-8859-1')
-		#print(domain_name)
-		dst_addr = socket.gethostbyname(domain_name)
-	dst_port = peer.recv(1)[0] << 8 | peer.recv(1)[0]
-	
-	print('Connecting to %s:%d' % (dst_addr, dst_port))
-	
-	remote = socket.socket()#(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-	remote.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	remote.bind((BIND_ADDR, BIND_PORT))
-	remote.connect((dst_addr,dst_port))
-	peer.sendall(b'\x05\x00\x00\x01\x00\x00\x00\x00\x00\x00')
-	
-	threading.Thread(target = pipe_send, args = (peer, remote)).start()
-	pipe_recv(peer, remote)
-	
-	print('End Connection')
-	
+    
+    #Authentication
+    ver = peer.recv(1)
+    nmethods = peer.recv(1)[0]
+    for i in range(nmethods):
+        method = peer.recv(1)
+    peer.sendall(b'\x05\x00')
+    
+    logger.info('Authentication succeeded.')
+    
+    #Connection
+    ver = peer.recv(1)
+    cmd = peer.recv(1)
+    rsv = peer.recv(1)
+    atyp = peer.recv(1)[0]
+    if atyp == 1: 
+        dst_addr = '%d.%d.%d.%d' % tuple(peer.recv(4))
+    elif atyp == 3:
+        len = peer.recv(1)[0]
+        domain_name = peer.recv(len).decode('iso-8859-1')
+        #logger.info(domain_name)
+        dst_addr = socket.gethostbyname(domain_name)
+    dst_port = peer.recv(1)[0] << 8 | peer.recv(1)[0]
+    
+    
+    peer.sendall(b'\x05\x00\x00\x01\x00\x00\x00\x00\x00\x00')
+    buffer = peer.recv(RECV_SIZE)
+
+    https_res = make_HTTPS_request(buffer)
+
+    if https_res != None:
+        # logger.info('https_res = %s', https_res)
+        logger.warning("HTTPS available. Switch to HTTPS...")
+        peer.sendall(https_res)
+    else:
+        logger.info('Connecting to %s:%d' % (dst_addr, dst_port))
+        remote = socket.socket()#(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        remote.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        remote.bind((BIND_ADDR, BIND_PORT))
+        remote.connect((dst_addr,dst_port))
+
+        thread = threading.Thread(target = pipe_send, args = (peer, remote, buffer))
+        thread.start()
+        pipe_recv(peer, remote)
+
+        thread.join()
+        remote.close()
+
+    peer.close()
+    
+    logger.info('End Connection')
+    
 def startProxy():
-	proxy = socket.socket()
-	proxy.bind((PROXY_ADDR, PROXY_PORT))
-	proxy.listen(BACKLOG)
-	
-	sniffer = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
-	sniffer.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 2**30)
-	sniffer.bind(('eth0', ETH_P_ALL))
-	
-	print('Start listening.')
-	while 1:
-		thread_proxy = threading.Thread(target = run, args = (proxy.accept()[0],))
-		thread_proxy.start()
-		standard_ttl = -1
-		while thread_proxy.is_alive():
-			pkt, sa_ll = sniffer.recvfrom(MTU)
-			if len(pkt) <= 0:
-				break
-			mac_header = struct.unpack("!6s6sH", pkt[0:14])
-			if mac_header[2] != 0x800: 
-				continue
-	
-			ip_header = pkt[14:34]
-			fields = struct.unpack("!BBHHHBBHII", ip_header)
-			
-			packed_ip_src = fields[8]
-			if is_local(packed_ip_src):
-				continue
-			iplen = fields[2]
-			ttl = fields[5]
-			ip_src = ip_header[12:16]
-			ip_dst = ip_header[16:20]
-			
-			if sa_ll[2] != socket.PACKET_OUTGOING and ttl != standard_ttl:
-			#	print("incoming - src=%s, dst=%s, frame len = %d, TTL = %d"
-			#	%(socket.inet_ntoa(ip_src), socket.inet_ntoa(ip_dst), iplen, ttl))
-				if standard_ttl == -1:
-					standard_ttl = ttl
-				else:
-					print("Hijack dected! TTL should be %d instaed of %d" % (standard_ttl, ttl))
+    proxy = socket.socket()
+    proxy.bind((PROXY_ADDR, PROXY_PORT))
+    proxy.listen(BACKLOG)
+    
+    sniffer = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
+    sniffer.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 2**30)
+    sniffer.bind(('eth0', ETH_P_ALL))
+    
+    logger.info('Start listening.')
+    while 1:
+        thread_proxy = threading.Thread(target = run, args = (proxy.accept()[0],))
+        thread_proxy.start()
+        standard_ttl = -1
+        while thread_proxy.is_alive():
+            pkt, sa_ll = sniffer.recvfrom(MTU)
+            if len(pkt) <= 0:
+                break
+            mac_header = struct.unpack("!6s6sH", pkt[0:14])
+            if mac_header[2] != 0x800: 
+                continue
+    
+            ip_header = pkt[14:34]
+            fields = struct.unpack("!BBHHHBBHII", ip_header)
+            
+            packed_ip_src = fields[8]
+            if is_local(packed_ip_src):
+                continue
+            iplen = fields[2]
+            ttl = fields[5]
+            ip_src = ip_header[12:16]
+            ip_dst = ip_header[16:20]
+            
+            if sa_ll[2] != socket.PACKET_OUTGOING and ttl != standard_ttl:
+            #    logger.info("incoming - src=%s, dst=%s, frame len = %d, TTL = %d"
+            #    %(socket.inet_ntoa(ip_src), socket.inet_ntoa(ip_dst), iplen, ttl))
+                if standard_ttl == -1:
+                    standard_ttl = ttl
+                else:
+                    logger.warning("Hijack dected! TTL should be %d instaed of %d" % (standard_ttl, ttl))
 
 def is_local(packed_ip):
-	return packed_ip >> 24 == 10
+    return packed_ip >> 24 == 10
 
 def main():
-	threading.Thread(target = os.system, args = ("polipo socksParentProxy=127.0.0.1:%d proxyPort=%d" % (PROXY_PORT, HTTP_PORT),)).start()
-	startProxy()
-	
+    threading.Thread(target = os.system, args = ("polipo socksParentProxy=127.0.0.1:%d proxyPort=%d" % (PROXY_PORT, HTTP_PORT),)).start()
+    startProxy()
+    
 if __name__ == "__main__":
     main()
--- isp.py
+++ isp.py
@@ -20,14 +20,122 @@ ENABLE = True
 SCRIPT = b'<script>alert("PHP is the best language!");</script>'
 SCRIPT_LEN = len(SCRIPT)
 
+
 sessions = {}
 conf.L3socket = L3RawSocket
 
-def is_http_request(s):
-    return s[:3] == b'GET' and s.find(b' HTTP/1.1') != -1
+PKT_ACCEPT = 'accept'
+PKT_MODIFY = 'modify'
+PKT_REJECT = 'reject'
 
-def get_session(ip, port):
-    S = (ip, port)
+class Modifier(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def is_http_request(s):
+        return s[:3] == b'GET' and s.find(b' HTTP/1.1') != -1
+
+    @staticmethod
+    def is_http_response(s):
+        return s[:9] == b'HTTP/1.1 '
+    
+    def send(self, data, sess):
+        console.warning('empty handler: send')
+
+    def recv(self, data, sess):
+        console.warning('empty handler: recv')
+
+class Redirector(Modifier):
+    REDIRECT = b'HTTP/1.1 302 Found\r\nLocation: http://www.google.com\r\n'
+
+    def __init__(self):
+        self.drop_PSH = False
+        self.monitored = False
+
+    def send(self, data, sess):
+        return PKT_REJECT
+
+    def recv(self, data, sess):
+        pkt_status = PKT_ACCEPT
+        raw = bytes(data[TCP].payload)
+        if Modifier.is_http_response(raw):
+            pkt_status = PKT_MODIFIED
+            data[TCP].payload = Redirector.REDIRECT
+            self.drop_PSH = True
+        elif self.drop_PSH and data[TCP].flags & TCP_PSH:
+            pass
+            # send a fake response to server
+            
+        return pkt_status
+
+class Insertor(Modifier):
+    def __init__(self):
+        self.offset = 0
+        self.threshold = 10**10
+        self.monitored = False
+
+    def send(self, data, sess):
+        pkt_status = PKT_ACCEPT
+        if data[TCP].flags == TCP_ACK | TCP_PSH and Modifier.is_http_request(bytes(data[TCP].payload)):
+            self.monitored = True
+        logger.info("summary = %s, seq = %s, ack = %s", data.summary(), data.seq - sess['ack_'], data.ack - sess['seq_'])
+        if data[TCP].ack >= self.threshold + len(data[TCP].payload):
+            if ENABLE:
+                data[TCP].ack -= self.offset
+            logger.info(f'modify ack, delta = {-self.offset}, modified results = ({data[TCP].seq- sess["ack_"]}, {data[TCP].ack - sess["seq_"]})')
+            pkt_status = PKT_MODIFY
+        return pkt_status
+
+    def recv(self, data, sess):
+        pkt_status = PKT_ACCEPT
+        if data[TCP].seq >= self.threshold:
+            if ENABLE:
+                data[TCP].seq += self.offset
+            logger.info(f'modify seq, delta = {self.offset}, modified results = ({data[TCP].seq - sess["seq_"]}, {data[TCP].ack - sess["ack_"]})')
+            pkt_status = PKT_MODIFY
+
+        if self.monitored:
+            raw = bytes(data[TCP].payload)
+            logger.info("raw = %s", raw)
+            
+            loc = raw.lower().find(b'</body>')
+            if raw:
+                logger.info('original data size = %s', len(raw))
+            if loc != -1 and self.monitored: # didn't consider the case where '</body>' itself is a string, e.g., "var x = '</body>';".
+        
+                # find the enclosing </body>
+                # should insert script before </body>
+                raw = raw[:loc] + SCRIPT + raw[loc:]
+                self.offset = SCRIPT_LEN
+                self.threshold = data[TCP].seq + self.offset
+        
+                logger.warning('forging fake packet')
+                if ENABLE:
+                    data[TCP].payload = raw
+                pkt_status = PKT_MODIFY
+        
+            if raw.find(b'Content-Length:') != -1 and self.monitored:
+                loc = raw.find(b'Content-Length:')
+                start = loc + 16
+                end = start
+                length = 0
+                while end < len(raw) and 48 <= raw[end] <= 57:
+                    length = length * 10 + raw[end] - 48
+                    end += 1
+                logger.info('length = %s', length)
+                length = bytes(str(length + SCRIPT_LEN), 'utf-8')
+                if ENABLE:
+                    data[TCP].payload = raw[:start] + length + raw[end:]
+                pkt_status = PKT_MODIFY
+                logger.warning('modified HTTP header')
+        return pkt_status
+
+def get_new_modifier():
+    return Insertor()
+
+def get_session(ip, port): 
+    S = (ip, port) 
     if S in sessions:
         sess = sessions[S]
         # logger.info("recorded time = %s, current = %s", sess['time'], time.perf_counter())
@@ -41,11 +149,11 @@ def get_session(ip, port):
 def add_session(ip, port):
     S = (ip, port)
     logger.warning('new session')
-    sess = {'offset': 0, 'FIN_count': 0, 'threshold': 10**10, 'seq_': 0, 'ack_': 0, 'monitored': False, 'time': time.perf_counter()}
+    sess = {'FIN_count': 0, 'seq_': 0, 'ack_': 0, 'time': time.perf_counter(), 'modifier': get_new_modifier()}
     sessions[S] = sess
 
 def callback(pkt):
-    modified = False
+    result = PKT_ACCEPT
     try:
         data = IP(pkt.get_payload())
         if data.proto == PROTO_TCP and data.sport == PORT_WWW_HTTP: # incoming
@@ -55,47 +163,10 @@ def callback(pkt):
                 sess['seq_'] = data[TCP].seq
                 sess['ack_'] = data[TCP].ack
 
-            if sess != None :
+            if sess != None:
 
-                logger.info("summary = %s, seq = %s, ack = %s, thres = %s", data.summary(), data.seq - sess['seq_'], data.ack - sess['ack_'], sess['threshold'] - sess['seq_'])
-                if data[TCP].seq >= sess['threshold']:
-                    if ENABLE:
-                        data[TCP].seq += sess['offset']
-                    logger.info(f'modify seq, delta = {sess["offset"]}, modified results = ({data[TCP].seq - sess["seq_"]}, {data[TCP].ack - sess["ack_"]})')
-                    modified = True
-
-                raw = bytes(data[TCP].payload)
-                # logger.info("raw = %s", raw)
-                loc = raw.find(b'</body>')
-                if raw:
-                    logger.info('original data size = %s', len(raw))
-                if loc != -1 and sess['monitored']: # didn't consider the case where '</body>' itself is a string, e.g., "var x = '</body>';".
-
-                    # find the enclosing </body>
-                    # should insert script before </body>
-                    raw = raw[:loc] + SCRIPT + raw[loc:]
-                    sess['offset'] = SCRIPT_LEN
-                    sess['threshold'] = data[TCP].seq + sess['offset']
-
-                    logger.warning('forging fake packet')
-                    if ENABLE:
-                        data[TCP].payload = raw
-                    modified = True
-
-                elif raw.find(b'Content-Length:') != -1 and sess['monitored']:
-                    loc = raw.find(b'Content-Length:')
-                    start = loc + 16
-                    end = start
-                    length = 0
-                    while end < len(raw) and 48 <= raw[end] <= 57:
-                        length = length * 10 + raw[end] - 48
-                        end += 1
-                    logger.info('length = %s', length)
-                    length = bytes(str(length + SCRIPT_LEN), 'utf-8')
-                    if ENABLE:
-                        data[TCP].payload = raw[:start] + length + raw[end:]
-                    modified = True
-                    logger.warning('modified HTTP header')
+                logger.info("summary = %s, seq = %s, ack = %s", data.summary(), data.seq - sess['seq_'], data.ack - sess['ack_'])
+                result = sess['modifier'].recv(data, sess)
 
                 if data[TCP].flags & TCP_FIN:
                     sess['FIN_count'] += 1
@@ -104,27 +175,26 @@ def callback(pkt):
             if data[TCP].flags == TCP_SYN:
                add_session(data.dst, data.sport)
             sess = get_session(data.dst, data.sport)
-            if sess != None and data[TCP].flags == TCP_ACK | TCP_PSH and is_http_request(bytes(data[TCP].payload)):
-                sess['monitored'] = True
             if sess != None:
-                logger.info("summary = %s, seq = %s, ack = %s", data.summary(), data.seq - sess['ack_'], data.ack - sess['seq_'])
-            if sess != None:
-                if data[TCP].ack >= sess['threshold'] + len(data[TCP].payload):
-                    if ENABLE:
-                        data[TCP].ack -= sess['offset']
-                    logger.info(f'modify ack, delta = {-sess["offset"]}, modified results = ({data[TCP].seq- sess["ack_"]}, {data[TCP].ack - sess["seq_"]})')
-                    modified = True
+                result = sess['modifier'].send(data, sess)
             
     except:
         logger.info('error')
         traceback.print_exc()
     
-    if modified and ENABLE:
-        del data[TCP].chksum
-        del data.len
-        del data[IP].chksum
-        pkt.set_payload(bytes(data))
-    pkt.accept()
+    if ENABLE:
+        if result == PKT_MODIFY:
+            del data[TCP].chksum
+            del data.len
+            del data[IP].chksum
+            pkt.set_payload(bytes(data))
+            pkt.accept()
+        elif result == PKT_REJECT:
+            pkt.reject()
+        else:
+            pkt.accept()
+    else:
+        pkt.accept()
 
 def callback2(pkt):
     pkt.accept()
